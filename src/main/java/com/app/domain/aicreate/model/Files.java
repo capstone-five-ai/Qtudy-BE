4135b8d9c866683046693a7c7522c6af547bf024
kimyechan <tioon74@naver.com>
2023-10-18 00:07:33 +0900

    AICREATE UPDATE

--- src/main/java/com/app/domain/aicreate/model/Files.java
+++ src/main/java/com/app/domain/aicreate/model/Files.java
@@ -1,4 +1,11 @@
 package com.app.domain.aicreate.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(schema = "Qtudy",name = "files")
 public class Files {
+
+
 }
