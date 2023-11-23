8a38738b9d067c5aadb703590e408ed85ec341d4
jungdonggyu <1013as@naver.com>
2023-11-23 10:11:25 +0900

    Feat: "사용자 생성 문제 생성, 수정, 삭제 기능 구현"

--- src/main/java/com/app/domain/memberSavedProblem/entity/MemberSavedProblem.java
+++ src/main/java/com/app/domain/memberSavedProblem/entity/MemberSavedProblem.java
@@ -4,6 +4,7 @@ import com.app.domain.common.BaseEntity;
 import com.app.domain.member.entity.Member;
 import com.app.domain.memberSavedProblem.constant.ProblemType;
 import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 import javax.persistence.*;
 import java.util.List;
@@ -12,8 +13,13 @@ import java.util.List;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
+//@SuperBuilder
 @Builder
 public class MemberSavedProblem extends BaseEntity {
+
+//    protected MemberSavedProblem(){
+//        super();
+//    }
     @Id
     @Column(name = "MEMBER_SAVED_PROBLEM_ID")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -38,10 +44,22 @@ public class MemberSavedProblem extends BaseEntity {
 
     @ElementCollection
     @CollectionTable(name = "PROBLEM_CHOCIE", joinColumns = @JoinColumn(name = "MEMBER_SAVED_PROBLEM_ID"))
-    @Column(name = "CHOICE_CONTETN")
-    private List<String> problemChocies;
+    @Column(name = "CHOICE_CONTENT")
+    private List<String> problemChoices;
 
     public void updateMember(Member member) {
         this.member = member;
+    }
+
+    public void updateProblemName(String problemName) {
+        this.problemName = problemName;
+    }
+
+    public void updateProblemAnswer(String problemAnswer) {
+        this.problemAnswer = problemAnswer;
+    }
+
+    public void updateProblemCommentary(String problemCommentary) {
+        this.problemCommentary = problemCommentary;
     }
 }
