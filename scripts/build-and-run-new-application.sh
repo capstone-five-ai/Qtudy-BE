f249a3e8a28d584bd51652b4daa982039cc91de3
yujamint <woawoa0513@gmail.com>
2024-06-02 02:05:37 +0900

    fix: script가 종료되지 않는 문제점 해결

--- scripts/build-and-run-new-application.sh
+++ scripts/build-and-run-new-application.sh
@@ -2,4 +2,4 @@ PROJECT_PATH=/home/ubuntu/Qtudy-BE
 BUILD_PATH=$PROJECT_PATH/build/libs
 BUILD_JAR=$BUILD_PATH/Qtudy-server-0.0.1-SNAPSHOT.jar
 
-nohup java -jar $BUILD_JAR --spring.profiles.active=stage /dev/null 2> /dev/null < /dev/null &
+nohup java -jar $BUILD_JAR --spring.profiles.active=stage > $PROJECT_PATH/nohup.out 2>&1 &
