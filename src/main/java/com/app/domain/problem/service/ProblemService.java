1b9afb44f50323f3259709927e47dc11ecb41ce7
yujamint <woawoa0513@gmail.com>
2024-07-11 00:55:06 +0900

    fix: AI 생성 문제 기반의 CategorizedProblem 수정 버그 해결

--- src/main/java/com/app/domain/problem/service/ProblemService.java
+++ src/main/java/com/app/domain/problem/service/ProblemService.java
@@ -4,6 +4,7 @@ import com.app.domain.problem.entity.Problem;
 import com.app.domain.problem.repository.ProblemRepository;
 import com.app.global.error.ErrorCode;
 import com.app.global.error.exception.EntityNotFoundException;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,6 +15,22 @@ import org.springframework.transaction.annotation.Transactional;
 public class ProblemService {
 
     private final ProblemRepository problemRepository;
+
+    public Problem updateProblem(Problem problem, Long problemId){
+        Problem preProblem = findVerifiedProblemByProblemId(problemId);
+        Optional.ofNullable(problem.getProblemName())
+                .ifPresent(problemName -> preProblem.updateProblemName(problemName));
+        Optional.ofNullable(problem.getProblemAnswer())
+                .ifPresent(problemAnswer -> preProblem.updateProblemAnswer(problemAnswer));
+        Optional.ofNullable(problem.getProblemCommentary())
+                .ifPresent(problemCommentary -> preProblem.updateProblemCommentary(problemCommentary));
+        Optional.ofNullable(problem.getProblemChoices())
+                .ifPresent(problemChoices ->{
+                    preProblem.getProblemChoices().clear();
+                    preProblem.getProblemChoices().addAll(problemChoices);
+                });
+        return problemRepository.save(preProblem);
+    }
 
     @Transactional(readOnly = true)
     public Problem findVerifiedProblemByProblemId(Long problemId) {
